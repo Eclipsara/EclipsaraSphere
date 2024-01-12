699be389378c0e203dee4ef78f564b0527e71476
Auth <111557161+AuthFrl@users.noreply.github.com>
2024-01-12 20:51:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import {Card, CardBody} from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
 import Twitter from "./components/Twitter";
 
 export default function App() {
   return (
-   <>
-   <div class="bg-zinc-900 min-h-screen text-white pt-56">
-   <Twitter />
-   </div>
-
-   </>
+    <div className="flex flex-wrap justify-center p-4">
+      {/* Grid items */}
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+        <Twitter />
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+        <Twitter />
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+        <Twitter />
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+        <Twitter />
+      </div>
+      <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2">
+        <Twitter />
+      </div>
+    </div>
   );
 }
+
